1140182289f1e125ee8979369e7c75992be6bb48
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-11-25 01:23:04 +0100

    create button component and navbar

--- clinicaveterinaria/src/components/NavBar/NavBar.js
+++ clinicaveterinaria/src/components/NavBar/NavBar.js
@@ -1,33 +1,45 @@
+import { useNavigate } from "react-router";
+import Button from "../Boton/Button";
+import "./NavBar.scss"
+
 const NavBar = () =>{
+    let navigate = useNavigate()
     return(
         <>
-            <p>Aqui va un navbar</p>
-            <ul>
-                <li> <a href="#">Conocenos</a>
-                    <ul>
-                        <li><a href="#">Quiens Somos</a></li>
-                        <li><a href="#">Nuestras Instalaciones</a></li>
-                        <li><a href="#">Trabaja con nosotros</a></li>
-                    </ul>
-                </li>
-                <li> <a href="#">Servicios</a>
-                    <ul>
-                        <li><a href="#">Cirugia General</a></li>
-                        <li><a href="#">Medicina preventiva</a></li>
-                        <li><a href="#">Medicina Interna</a></li>
-                    </ul>
-                </li>
-                <li> <a href="#">Campaña</a>
-                    <ul>
-                        <li><a href="#">Higien Dental</a></li>
-                        <li><a href="#">Esterilizacion</a></li>
-                        <li><a href="#">Tos de las Perreras</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Conocenos</a>
-                </li>
-            </ul>
+        <div className="navbar_conteiner">
+            <nav className="navbar">
+                <ul className="navbar_link">
+                    <li> <a href="#">Conocenos</a>
+                        <ul className="navbar_ul">
+                            <li><a href="#">Quienes Somos</a></li>
+                            <li><a href="#">Nuestras Instalaciones</a></li>
+                            <li><a href="#">Trabaja con nosotros</a></li>
+                        </ul>
+                    </li>
+                    <li> <a href="#">Servicios</a>
+                        <ul className="navbar_ul" >
+                            <li><a href="#">Cirugia General</a></li>
+                            <li><a href="#">Medicina preventiva</a></li>
+                            <li><a href="#">Medicina Interna</a></li>
+                        </ul>
+                    </li>
+                    <li> <a href="#">Campaña</a>
+                        <ul className="navbar_ul" >
+                            <li><a href="#">Higien Dental</a></li>
+                            <li><a href="#">Esterilizacion</a></li>
+                            <li><a href="#">Tos de las Perreras</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Conocenos</a>
+                    </li>
+                </ul>
+            </nav>
+            <div>
+                <Button onClick={()=>navigate('/login')} >Sing in</Button>
+                <Button onClick={()=>navigate('/register')} >Sing up</Button>
+            </div>
+        </div>
         </>
     )
 };
