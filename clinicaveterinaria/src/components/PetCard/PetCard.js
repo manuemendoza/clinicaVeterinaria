967714fe49d8d5f5b55f691069c739138787132e
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-11-27 12:37:28 +0100

    created petCard

---
+++ clinicaveterinaria/src/components/PetCard/PetCard.js
@@ -0,0 +1,12 @@
+const PetCard = (props) => {
+    return(
+        <>
+        <div>
+            <h2>Nombre: Zeus</h2>
+            <p></p>
+        </div>
+        </>
+    )
+};
+
+export default PetCard
