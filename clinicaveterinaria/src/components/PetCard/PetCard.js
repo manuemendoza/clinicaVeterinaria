407da28f6135ec7423f3250bf95534e745eeec6d
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-11-27 13:23:47 +0100

    crreated petCard

--- clinicaveterinaria/src/components/PetCard/PetCard.js
+++ clinicaveterinaria/src/components/PetCard/PetCard.js
@@ -1,9 +1,20 @@
+import { useNavigate } from "react-router";
+import Button from "../Boton/Button";
+
 const PetCard = (props) => {
+    const  { pet } = props
+    const navigate = useNavigate()
+    const redirection = () =>{navigate('/appoinment')}
     return(
         <>
         <div>
-            <h2>Nombre: Zeus</h2>
-            <p></p>
+            <div>
+                <h2>Nombre: {/*{pet.name}*/}</h2>
+                <p>genero: {/*{pet.gender}*/}</p>
+                <p>Especie: {/*{pet.breed}*/}</p>  
+                <p>Raza: {/*{pet.kind}*/}</p>
+            </div>
+            <Button onClick={() => redirection()} >Pide una cita</Button> 
         </div>
         </>
     )
